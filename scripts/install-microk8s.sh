9c631dee4a9ac0348859c60376888c5a5d8012c9
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-05-08 13:04:08 -0500

    Add MicroK8s install script

---
+++ scripts/install-microk8s.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update the system
+sudo apt update -y
+
+# Install MicroK8s
+sudo snap install microk8s --classic
+
+# Add the current user to the 'microk8s' group
+sudo usermod -aG microk8s $USER
+
+# Create .kube directory if it doesn't exist
+mkdir -p ~/.kube
+
+# Change ownership of .kube directory to the current user
+sudo chown -f -R $USER ~/.kube
+
+# Add MicroK8s alias to the user's bashrc file if it's not already added
+if ! grep -q "alias kubectl='microk8s kubectl'" ~/.bashrc; then
+    echo "alias kubectl='microk8s kubectl'" >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+# Enable required MicroK8s services
+microk8s enable dashboard dns registry istio
+
+# Output instructions for post-installation steps
+echo "MicroK8s installed successfully."
+echo "You may need to log out and log back in for group membership changes to take effect."
+echo "Happy Kubernetting!"
