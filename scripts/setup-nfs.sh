744372aa8182f0014ab6ae7dacf32d8974332e4a
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-05-12 23:09:11 -0500

    Refactor NFS setup script and manifest files

--- scripts/setup-nfs.sh
+++ scripts/setup-nfs.sh
@@ -6,6 +6,17 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+
+# Enable Helm3 and add the CSI Driver for NFS repository
+microk8s enable helm3
+microk8s helm3 repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
+microk8s helm3 repo update
+
+# Install the NFS driver
+microk8s helm3 install csi-driver-nfs csi-driver-nfs/csi-driver-nfs \
+    --namespace kube-system \
+    --set kubeletDir=/var/snap/microk8s/common/var/lib/kubelet
+
 # Asignar el argumento proporcionado a una variable
 nfs_server_ip=$1
 
@@ -30,9 +41,7 @@ EOF
 microk8s kubectl apply --force -f manifests/env/01-configmap-nfs-client.yml
 
 # Delete existing StorageClass and PersistentVolumeClaim if they exist
-microk8s kubectl delete sc nfs-csi --ignore-not-found=true
-microk8s kubectl delete pvc nfs-pvc --ignore-not-found=true
-
+microk8s kubectl delete -f manifests/04-sc-pvc-nfs.yml --ignore-not-found=true
 # Apply the StorageClass and PersistentVolumeClaim manifest
 microk8s kubectl apply --force -f manifests/04-sc-pvc-nfs.yml
 
