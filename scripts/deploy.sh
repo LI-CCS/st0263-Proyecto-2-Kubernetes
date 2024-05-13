deaae6650f407879d1f2caccd7c8fd5aed425ebc
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-05-12 23:43:47 -0500

    Refactor deploy script and apply kustomization files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,13 @@
 # Delete manifest files if they exist
 microk8s kubectl delete -f manifests/ --ignore-not-found=true
 
+# Delete kustomization files if they exist
+microk8s kubectl delete -k . --ignore-not-found=true
+
 # Apply manifest files
 microk8s kubectl apply -f manifests/
+
+# Apply kustomization files
+microk8s kubectl apply -k .
 
 echo "Manifest files applied."
