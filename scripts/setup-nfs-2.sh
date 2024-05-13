179788e235628335b7b8837136ac5f464aaa4f5e
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-05-12 22:39:31 -0500

    Update NFS server IP in StorageClass and PersistentVolumeClaim manifest

--- scripts/setup-nfs-2.sh
+++ scripts/setup-nfs-2.sh
@@ -9,31 +9,17 @@ fi
 # Asignar el argumento proporcionado a una variable
 nfs_server_ip=$1
 
-# Create the env directory if it doesn't exist
-mkdir -p manifests/env
-
-# Delete existing ConfigMap if it exists
-microk8s kubectl delete configmap nfs-server-config --ignore-not-found=true
-
-
-# Create the ConfigMap manifest
-cat <<EOF > manifests/env/01-configmap-nfs-client.yml
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: nfs-server-config
-data:
-  NFS_SERVER_IP: $nfs_server_ip
-EOF
-
-# Apply the ConfigMap manifest
-microk8s kubectl apply --force -f manifests/env/01-configmap-nfs-client.yml
-
 # Delete existing StorageClass and PersistentVolumeClaim if they exist
 microk8s kubectl delete sc nfs-csi --ignore-not-found=true
 microk8s kubectl delete pvc nfs-pvc --ignore-not-found=true
 
+# Create if it does not exist the temporary directory for the NFS server
+mkdir -p manifests/tmp
+
+# Update the NFS server IP in the StorageClass and PersistentVolumeClaim manifest
+cat manifests/04-sc-pvc-nfs.yml | sed "s/NFS_SERVER_IP/$nfs_server_ip/g" > manifests/temp/04-sc-pvc-nfs.yml.tmp
+
 # Apply the StorageClass and PersistentVolumeClaim manifest
-microk8s kubectl apply --force -f manifests/04-sc-pvc-nfs.yml
+microk8s kubectl apply --force -f manifests/04-sc-pvc-nfs.yml.tmp
 
 echo "NFS configuration completed."
